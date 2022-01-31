5b9f13eecd8141687947df8aa6589729f860fd1d
derya <derya@boonandmoil.dev>
2022-01-31 11:34:12 +0000

    changes tool once user selects options

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,24 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     let tools = ["Rock", "Paper", "Scissors"]
-    var selected = Int.random(in: 0..<3)
-    var win = Bool.random()
+    @State var selected = Int.random(in: 0..<3)
+    @State var isWinner = Bool.random()
+    @State var output = "No selection made"
     
     var body: some View {
         VStack{
             Button(tools[selected]) {}
-            Text(win ? "Win" : "Lose")
+            Text(isWinner ? "Win" : "Lose")
             
             HStack {
-                Button("Rock") {}
-                Button("Paper") {}
-                Button("Scissors") {}
+                Button("Rock") {
+                    selectTool("Rock")
+                }
+                Button("Paper") {
+                    selectTool("Paper")
+                }
+                Button("Scissors") {
+                    selectTool("Scissors")
+                }
             }
             
             Text("Your score")
             Text("0")
+            Text(output)
         }
 
+    }
+    
+    func selectTool(_ tool: String) {
+        output = "You've selected \(tool)"
+        selected = Int.random(in: 0..<3)
     }
 }
 

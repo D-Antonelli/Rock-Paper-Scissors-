c4d68118323fc289ff8c4aac9969dd38a765d331
derya <derya@boonandmoil.dev>
2022-02-07 09:51:42 +0000

    adds game over logic after total number of questions is asked

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
     let losingMoves = ["Scissors", "Rock", "Paper"]
     
     @State private var score = 0
-
+    @State private var numberOfQuestions = 3;
+    @State private var isGameOver = false;
     @State private var selected = Int.random(in: 0..<3)
     @State private var shouldWin = Bool.random()
     @State private var output = "No selection made"
@@ -38,7 +39,11 @@ struct ContentView: View {
             Text(output)
             Text("Your score is \(score)")
         }
-
+        .alert("Game Over", isPresented: $isGameOver) {
+            Button("Restart", action: restartGame)
+        } message: {
+            Text("Your total score is \(score)")
+        }
     }
     
     func selectMove(_ move: String) {
@@ -51,14 +56,25 @@ struct ContentView: View {
         }
         
         shuffle()
+        numberOfQuestions -= 1
+        
+        if(numberOfQuestions == 0) {
+            isGameOver = true;
+            return
+        }
     }
     
 
-
-func shuffle() {
-    selected = Int.random(in: 0..<3)
-    shouldWin = Bool.random()
-}
+    func shuffle() {
+        selected = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+    }
+    
+    func restartGame() {
+        score = 0
+        numberOfQuestions = 3
+        shuffle()
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
